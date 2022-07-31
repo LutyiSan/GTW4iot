fc5d307e6a25ffbc10bf8bd5d2b0d31520589546
LutyiSan <76662954+LutyiSan@users.noreply.github.com>
2022-07-31 14:04:06 +0300

    Update csv_to_dict.py

--- csv_to_dict.py
+++ csv_to_dict.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def get_device_dict(file):
-    device_dict = pd.read_csv(f'devices/{file}', delimiter=";", index_col=False).to_dict('list')
+    device_dict = pd.read_csv(file, delimiter=";", index_col=False).to_dict('list')
     return device_dict
