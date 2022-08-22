d4312ee2809678a8d5ef14136d6bf8475c203f14
LutyiSan <76662954+LutyiSan@users.noreply.github.com>
2022-08-22 15:47:23 +0300

    Update csv_to_dict.py

--- gtw/csv_to_dict.py
+++ gtw/csv_to_dict.py
@@ -2,7 +2,7 @@ import pandas as pd
 from loguru import logger
 
 PREFIX = "gtw/devices/"
-def get_device_dict(,file):
+def get_device_dict(file):
     try:
         device_dict = pd.read_csv(f"{PREFIX}{file}", delimiter=";", index_col=False).to_dict('list')
         return device_dict
